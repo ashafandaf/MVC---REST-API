37a07735622d9168a2b800dbd3f42eadc72ae3cf
Ashfan Dafa <ashfandafa12@gmail.com>
2026-01-02 08:27:15 +0700

    PerpustakaanAppMVC

---
+++ PerpustakaanAppMVC/Model/Repository/MahasiswaRestApiRepository.cs
@@ -0,0 +1,82 @@
+﻿using PerpustakaanAppMVC.Model.Entity;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerpustakaanAppMVC.Model.Repository
+{
+    public class MahasiswaRestApiRepository
+    {
+
+        private readonly string _baseUrl = "http://latihan.coding4ever.net:5555/";
+
+        /* =======================
+           READ ALL (Latihan 3)
+           ======================= */
+        public List<Mahasiswa> ReadAll()
+        {
+            var client = new RestClient(_baseUrl);
+            var request = new RestRequest("api/mahasiswa", Method.GET);
+
+            var response = client.Execute<List<Mahasiswa>>(request);
+            return response.Data;
+        }
+
+        /* =======================
+           READ BY NAMA (Latihan 4)
+           ======================= */
+        public List<Mahasiswa> ReadByNama(string nama)
+        {
+            var client = new RestClient(_baseUrl);
+            var request = new RestRequest("api/mahasiswa", Method.GET);
+            request.AddParameter("nama", nama);
+
+            var response = client.Execute<List<Mahasiswa>>(request);
+            return response.Data;
+        }
+
+        /* =======================
+           CREATE (Latihan 5)
+           ======================= */
+        public bool Create(Mahasiswa mhs)
+        {
+            var client = new RestClient(_baseUrl);
+            var request = new RestRequest("api/mahasiswa", Method.POST);
+            request.AddJsonBody(mhs);
+
+            var response = client.Execute(request);
+            return response.IsSuccessful;
+        }
+
+        /* =======================
+           UPDATE
+           ======================= */
+        public bool Update(Mahasiswa mhs)
+        {
+            var client = new RestClient(_baseUrl);
+            var request = new RestRequest($"api/mahasiswa/{mhs.Npm}", Method.PUT);
+            request.AddJsonBody(mhs);
+
+            var response = client.Execute(request);
+            return response.IsSuccessful;
+        }
+
+        /* =======================
+           DELETE
+           ======================= */
+        public bool Delete(string nim)
+        {
+            var client = new RestClient(_baseUrl);
+            var request = new RestRequest($"api/mahasiswa/{nim}", Method.DELETE);
+
+            var response = client.Execute(request);
+            return response.IsSuccessful;
+        }
+
+
+    }
+}
+    
